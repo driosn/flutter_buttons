03ffea142d8d94c93055f7cc8f67801810eaee80
David Rios <davidsamuelrios07@gmail.com>
2020-01-31 08:51:19 -0400

    Flutter buttons

--- lib/main.dart
+++ lib/main.dart
@@ -9,103 +9,146 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: ButtonsApp(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class ButtonsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text('Flutter Buttons'),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+        child: Container(
+          width: 250,
+          child: Column(
+            children: <Widget>[
+              flatButton(),
+              SizedBox(height: 20.0,),
+              flatButtonWithIcon(),
+              SizedBox(height: 20.0,),
+              raisedButton(),
+              SizedBox(height: 20.0,),
+              raisedButtonWithIcon(),
+              SizedBox(height: 20.0,),
+              iconButton(),
+              SizedBox(height: 20.0,),
+              outlineButton(),
+              SizedBox(height: 20.0,),
+              outlineButtonWithIcon(),
+              SizedBox(height: 20.0,),
+              customButton(),
+              SizedBox(height: 20.0,),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: floatingActionButton(),
+    );
+  }
+
+  Widget flatButton() {
+    return FlatButton(
+      onPressed: () { print('Soy un boton :v'); },
+      child: Text('FlatButton'),
+      textColor: Colors.blue,
+    );
+  }
+
+  Widget flatButtonWithIcon() {
+    return FlatButton.icon(
+      onPressed: () { print('Soy un boton :');},
+      icon: Icon(Icons.face),
+      label: Text('Flat Button with Icon'),
+      textColor: Colors.blue,
+    );
+  }
+
+  Widget raisedButton() {
+    return RaisedButton(
+      onPressed: () { print('Soy un boton :v');},
+      child: Text('RaisedButton'),
+      color: Colors.red,
+      textColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(20.0),
+      ),
+    );
+  }
+
+  Widget raisedButtonWithIcon() {
+    return RaisedButton.icon(
+      onPressed: () { print('Soy un boton :v'); },
+      label: Text('Raised Button With Icon'),
+      icon: Icon(Icons.face),
+      color: Colors.red,
+      textColor: Colors.white,
+      shape: RoundedRectangleBorder(),
+    );
+  }
+
+  Widget floatingActionButton() {
+    return FloatingActionButton(
+      onPressed: () { print('Soy un boton'); },
+      child: Icon(Icons.add),
+    );
+  }
+
+  Widget iconButton() {
+    return IconButton(
+      onPressed: () { print('Soy un iconButton :v');},
+      icon: Icon(Icons.group_add),
+      color: Colors.green,
+      iconSize: 75.0,
+    );
+  }  
+
+  Widget outlineButton() {
+    return OutlineButton(
+      onPressed: () { print('Outline Button :v'); },
+      child: Text('OutlineButton'),
+      textColor: Colors.deepPurple,
+    );
+  }
+
+  Widget outlineButtonWithIcon() {
+    return OutlineButton.icon(
+      onPressed: () { print('Boton'); },
+      label: Text('Outline Button With Icon'),
+      icon: Icon(Icons.face),
+      textColor: Colors.deepPurple,
+    );
+  }
+
+  Widget customButton() {
+    return InkWell(
+      child: Container(
+        height: 50.0,
+        width: 100.0,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          color: Colors.indigo,
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+              blurRadius: 0.8,
+              color: Colors.grey,
+              spreadRadius: 0.8,
+              offset: Offset(8, 8)
+            )
+          ]
+        ),
+        child: Center(
+          child: Icon(Icons.face,
+           color: Colors.white,
+          )
+        ),
+      ) ,
+      onTap: () {},
     );
   }
 }
